3654d49c4f31df2a23fc9001cb9767f52f04d639
lopugit <ireply@myleisure.com.au>
2019-04-09 14:29:41 +1000

    ready

--- dist/smarts.js
+++ dist/smarts.js
@@ -886,7 +886,7 @@ module.exports = function () {
       // If the path array has only 1 more element, we've reached
       // the intended property and set its value
       if (propsArray.length == 1) {
-        if (that.getsmart(vue, 'reactiveSetter', false) && (that.$set || that.getsmart(vue, '$set', false))) {
+        if (that.getsmart(vue, 'reactiveSetter', false) && that.$set) {
           that.$set(obj, propsArray[0], value);
         } else {
           obj[propsArray[0]] = value;
