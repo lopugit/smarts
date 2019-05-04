29c93df97d7ea0e9daabc8950fc1e8bc04166a2d
lopugit <ireply@myleisure.com.au>
2019-05-05 07:22:29 +1000

    ready

--- dist/smarts.js
+++ dist/smarts.js
@@ -1155,5 +1155,36 @@ module.exports = function () {
     } else {
       return defaultValue;
     }
+  }), _defineProperty(_ref16, "equals", function equals(obj1, obj2) {
+    if (obj1 && obj2 && _typeof(obj1) == 'object' && _typeof(obj2) == 'object') {
+      //Loop through properties in object 1
+      for (var p in obj1) {
+        //Check property exists on both objects
+        if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+
+        switch (_typeof(obj1[p])) {
+          //Deep compare objects
+          case 'object':
+            if (!this.equals(obj1[p], obj2[p])) return false;
+            break;
+          //Compare function code
+
+          case 'function':
+            if (typeof obj2[p] == 'undefined' || p != 'compare' && obj1[p].toString() != obj2[p].toString()) return false;
+            break;
+          //Compare values
+
+          default:
+            if (obj1[p] != obj2[p]) return false;
+        }
+      } //Check object 2 for any extra properties
+
+
+      for (var p in obj2) {
+        if (typeof obj1[p] == 'undefined') return false;
+      }
+
+      return true;
+    }
   }), _ref16;
 };
