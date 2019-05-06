27924a57628cfb14ce741219f7551e658d74a1e0
lopugit <ireply@myleisure.com.au>
2019-05-07 03:00:20 +1000

    ready

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   presets: [
-    '@babel/preset-env'
-	],
-	useBuiltIns: 'usage'
+		[
+			'@babel/preset-env',
+			{
+				useBuiltIns: 'usage'
+			}
+		]
+	]
 }
--- dist/smarts.js
+++ dist/smarts.js
@@ -1,5 +1,35 @@
 "use strict";
 
+require("core-js/modules/es6.array.for-each");
+
+require("core-js/modules/es6.array.filter");
+
+require("core-js/modules/es6.object.keys");
+
+require("core-js/modules/es6.object.define-property");
+
+require("core-js/modules/es6.promise");
+
+require("core-js/modules/es6.regexp.split");
+
+require("core-js/modules/es6.array.index-of");
+
+require("core-js/modules/es7.symbol.async-iterator");
+
+require("core-js/modules/es6.symbol");
+
+require("core-js/modules/web.dom.iterable");
+
+require("core-js/modules/es6.array.iterator");
+
+require("regenerator-runtime/runtime");
+
+require("core-js/modules/es6.regexp.to-string");
+
+require("core-js/modules/es6.date.to-string");
+
+require("core-js/modules/es6.object.to-string");
+
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
