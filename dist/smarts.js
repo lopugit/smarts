200d0a5d957ca6d3f447310fcf4a20478f807eaa
lopugit <ireply@myleisure.com.au>
2019-05-09 22:57:19 +1000

    ready

--- dist/smarts.js
+++ dist/smarts.js
@@ -28,8 +28,6 @@ require("core-js/modules/es6.date.to-string");
 
 require("core-js/modules/es6.object.to-string");
 
-var _this = void 0;
-
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
@@ -78,7 +76,7 @@ module.exports = function () {
       return val;
     },
     dupe: function dupe(obj) {
-      return f.parse(f.stringify(obj, _this.stringifyFunc), _this.parseFunc);
+      return f.parse(f.stringify(obj, this.stringifyFunc), this.parseFunc);
     },
     mod: function mod(args, _mod) {
       return _mod(args) || args;
@@ -999,7 +997,7 @@ module.exports = function () {
       // If the path array has only 1 more element, we've reached
       // the intended property and set its value
       if (propsArray.length == 1) {
-        if (that.getsmart(vue, 'reactiveSetter', false) && that.$set) {
+        if (that.getsmart(vue, 'reactiveSetter', false) && that.$set && obj) {
           that.$set(obj, propsArray[0], value);
 
           if (typeof that.getsmart(window, '$store.commit', undefined) == 'function') {
@@ -1024,7 +1022,7 @@ module.exports = function () {
 
       if (_typeof(obj[propsArray[0]]) !== 'object') {
         // If we have reached an undefined/null property
-        if (that.getsmart(vue, 'reactiveSetter', false) && that.$set) {
+        if (that.getsmart(vue, 'reactiveSetter', false) && that.$set && obj) {
           that.$set(obj, propsArray[0], {});
 
           if (typeof that.getsmart(window, '$store.commit', undefined) == 'function') {
@@ -1041,7 +1039,7 @@ module.exports = function () {
     if (property) {
       return deepGetByArray(obj, property, value);
     } else {
-      if (that.getsmart(vue, 'reactiveSetter', false) && that.$set) {
+      if (that.getsmart(vue, 'reactiveSetter', false) && that.$set && obj) {
         that.$set(obj, undefined, value);
 
         if (typeof that.getsmart(window, '$store.commit', undefined) == 'function') {
@@ -1076,26 +1074,26 @@ module.exports = function () {
       return this.getsmart(get, 'value', get);
     }
   }), _defineProperty(_ref16, "vgosmart", function vgosmart(obj, property, value, context) {
-    var _this2 = this;
+    var _this = this;
 
     // stands for v-model get or set smart
     // return value from property path, either gotten or smartly set
     return {
       get: function get() {
-        var get = _this2.getsmart(obj, property, value, true);
+        var get = _this.getsmart(obj, property, value, true);
 
         if (get.undefined) {
-          get = _this2.setsmart(obj, property, get.value, context);
+          get = _this.setsmart(obj, property, get.value, context);
         }
 
         if (context) {
           return get;
         } else {
-          return _this2.getsmart(get, 'value', get);
+          return _this.getsmart(get, 'value', get);
         }
       },
       set: function set(val) {
-        _this2.setsmart(obj, property, val);
+        _this.setsmart(obj, property, val);
       }
     };
   }), _defineProperty(_ref16, "getsmartval", function getsmartval(obj, property, defaultValue) {
@@ -1118,7 +1116,7 @@ module.exports = function () {
   }), _defineProperty(_ref16, "safeparse", function safeparse(something) {
     return this.jsmart.parse(something || '');
   }), _defineProperty(_ref16, "mapsmart", function mapsmart(list) {
-    var _this3 = this;
+    var _this2 = this;
 
     var keyProperty = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'title';
     var returnExistant = arguments.length > 2 ? arguments[2] : undefined;
@@ -1128,7 +1126,7 @@ module.exports = function () {
         reject();
       } else if (list) {
         if (list.length == 0) {
-          if (returnExistant && _this3.getsmart(list, 'mapped.' + returnExistant, false) || !returnExistant) {
+          if (returnExistant && _this2.getsmart(list, 'mapped.' + returnExistant, false) || !returnExistant) {
             resolve(true);
           } else if (returnExistant) {
             resolve(false);
@@ -1138,8 +1136,8 @@ module.exports = function () {
         }
 
         if (!list.mapped || typeof list.mapped === 'boolean') {
-          if (_this3.getsmart(local.vue, 'reactiveSetter', false) && _this3.$set) {
-            _this3.$set(list, 'mapped', {});
+          if (_this2.getsmart(local.vue, 'reactiveSetter', false) && _this2.$set && list) {
+            _this2.$set(list, 'mapped', {});
           } else {
             list['mapped'] = {};
           }
@@ -1147,14 +1145,14 @@ module.exports = function () {
 
         for (var i = 0; i < list.length; i++) {
           if (typeof list[i] !== 'string') {
-            if (_this3.getsmart(local.vue, 'reactiveSetter', false) && _this3.$set) {
-              _this3.$set(list.mapped, list[i][keyProperty], list[i]);
+            if (_this2.getsmart(local.vue, 'reactiveSetter', false) && _this2.$set && list.mapped) {
+              _this2.$set(list.mapped, list[i][keyProperty], list[i]);
             } else {
               list['mapped'][list[i][keyProperty]] = list[i];
             }
 
             if (i == list.length - 1) {
-              if (returnExistant && _this3.getsmart(list, 'mapped.' + returnExistant, false) || !returnExistant) {
+              if (returnExistant && _this2.getsmart(list, 'mapped.' + returnExistant, false) || !returnExistant) {
                 resolve(true);
               } else if (returnExistant) {
                 resolve(false);
@@ -1187,7 +1185,7 @@ module.exports = function () {
           //   } 
           // } 
           else if (i == list.length - 1) {
-              if (returnExistant && _this3.getsmart(list, 'mapped.' + returnExistant, false) || !returnExistant) {
+              if (returnExistant && _this2.getsmart(list, 'mapped.' + returnExistant, false) || !returnExistant) {
                 resolve(true);
               } else if (returnExistant) {
                 resolve(false);
