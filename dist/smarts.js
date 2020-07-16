14eb17baf49fe23939e42c78ddadd0e9283e06eb
lopugit <ireply@myleisure.com.au>
2020-07-16 22:02:53 +1000

    version bump to 1.0.215+1 patch version

--- dist/smarts.js
+++ dist/smarts.js
@@ -1678,8 +1678,8 @@ module.exports = ({
       return path;
     },
 
-    ppa(path = "") {
-      return this.parsePropertyArray(path);
+    ppa(pathArray) {
+      return this.parsePropertyArray(pathArray);
     },
 
     setsmart(obj, property, value, context) {
