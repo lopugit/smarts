62044ede4cf8d8d79fbb6865e555a17c35197553
lopugit <ireply@myleisure.com.au>
2020-07-18 22:04:55 +1000

    Adding parse tests for nested circular objects and nested smart unsupported parsing by JSON.parse() such as undefined and functions

--- test/tests/functions/parse.js
+++ test/tests/functions/parse.js
@@ -23,9 +23,60 @@ describe("Function smarts.parse tests", ()=>{
 		expect(obj.self).to.equal(obj)
 	})
 
-	test("Should parse value that was undefined", ()=>{
+	test("should parse nested basic circular object without error", ()=>{
+
+		let string = '[{"test":"1","nested":"2"},"hey",{"self": "0"}]'
+		let obj = null
+		let err = false
+
+		try {
+			obj = smarts.parse(string)
+		} catch(e){
+			console.error(e)
+			err = e
+		}
+		
+		expect(err).to.equal(false)
+		expect(obj.nested.self).to.equal(obj)
+	})
+
+	test("should parse circular object that points to nested object without error", ()=>{
+
+		let string = '[{"test":"1","nested":"2"},"hey",{"self": "2"}]'
+		let obj = null
+		let err = false
+
+		try {
+			obj = smarts.parse(string)
+		} catch(e){
+			console.error(e)
+			err = e
+		}
+		
+		expect(err).to.equal(false)
+		expect(obj.nested.self).to.equal(obj.nested)
+	})
+
+	test("Should parse property of value that was undefined", ()=>{
 		
 		let string = '[{"property":"1"},{"type":"2","$js":"2"},"undefined"]'
+		let obj = { property: '' }
+		let err = false
+		'[{"nested":"1"},{"property":"2"},{"type":"3","$js":"3"},"undefined"]'
+		try {
+			obj = smarts.parse(string)
+		} catch(e){
+			console.error(e)
+			err = e
+		}
+		
+		expect(err).to.equal(false)
+		expect(obj.property).to.equal(undefined)
+		
+	})
+	test("Should parse property of nested property of value that was undefined", ()=>{
+		
+		let string = '[{"nested":"1"},{"property":"2"},{"type":"3","$js":"3"},"undefined"]'
 		let obj = { property: '' }
 		let err = false
 		
@@ -37,7 +88,7 @@ describe("Function smarts.parse tests", ()=>{
 		}
 		
 		expect(err).to.equal(false)
-		expect(obj.property).to.equal(undefined)
+		expect(obj.nested.property).to.equal(undefined)
 		
 	})
 })
