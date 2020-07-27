254f770e8d4803d0b9e78ff041083c4ba6986d21
lopugit <ireply@myleisure.com.au>
2020-07-27 20:05:38 +1000

    Added basic value test for adding non-existant basic values

--- test/tests/functions/merge.js
+++ test/tests/functions/merge.js
@@ -54,6 +54,30 @@ describe("Function smarts.merge tests", ()=>{
 		expect(err).to.equal(false)
 		
 	})
+	test("obj2 basic values should be added if not present", ()=>{
+
+		let obj1 = {
+		}
+
+		let obj2 = {
+			foo: 'bar2'
+		}
+
+		let err = false
+
+		try {
+			smarts.merge(obj1, obj2, {
+				overwrite: true
+			})
+		} catch(e){
+			console.error(e)
+			err = e
+		}
+		
+		expect(obj1.foo).to.equal('bar2')
+		expect(err).to.equal(false)
+		
+	})
 	test("obj1 objects should only have obj2 properties added, not overwritten in any case", ()=>{
 
 		let obj1 = {
